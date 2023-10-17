0078cc3eb81e5b4b828f18de828e6a82bfb66ec2
Davies Dev <davidonzan@gmail.com>
2023-10-17 11:36:04 +0100

    Remove login required permission

--- chat/views.py
+++ chat/views.py
@@ -10,13 +10,13 @@ openai.api_key = settings.OPENAI_API_KEY
 
 # Create your views here.
 class PromptRequestView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = PromptRequestSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        pre = "Let's have a doctor - patient conversation"
+        pre = "Let's have a doctor - patient conversation\n"
         response = openai.Completion.create(
             model="text-davinci-003", prompt=pre + serializer.validated_data["prompt"]
         )
